20fe22bdef349beaf0d7a4d5b9c6bd3e72ea8782
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 12:31:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,11 +17,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-
   cheese_types.each do |cheese|
-
     return cheese if array.include?(cheese)
-
   end
-  nil
 end
